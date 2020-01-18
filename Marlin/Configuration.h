5a3a9bb882bb456897438b17ca698bb08ce53f67
mrflobow <flobow@outlook.de>
2020-01-18 16:24:41 +0100

    Updated Hemera defaults with a little esteps finetuning

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -730,14 +730,14 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4[, E5]]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 419 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 413 }
 
 /**
  * Default Max Feed Rate (mm/s)
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4[, E5]]]]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 500, 500, 5, 25 }
+#define DEFAULT_MAX_FEEDRATE          { 500, 500, 5, 40 }
 
 /**
  * Default Max Acceleration (change/s) change = mm/s
@@ -745,7 +745,7 @@
  * Override with M201
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4[, E5]]]]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 500, 500, 100, 5000 }
+#define DEFAULT_MAX_ACCELERATION      { 500, 500, 100, 8000 }
 
 /**
  * Default Acceleration (change/s) change = mm/s
